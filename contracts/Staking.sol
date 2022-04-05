fea83e300201aa0f1cb50393dfb0138d956813da
moribus-single <dvg37@tpu.ru>
2022-04-05 16:05:00 +0700

    refactor

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,9 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "./IStaking.sol";
-import "./Token.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
