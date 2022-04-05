3aec95b6b9dbf4fc2836009c1c44da771d417dd9
moribus-single <dvg37@tpu.ru>
2022-04-05 15:08:23 +0700

    feat: ownable

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,21 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ERC20 token for staking protocol
  */
-contract Token is ERC20{
+contract Token is ERC20, Ownable{
     constructor(
         string memory name,
         string memory symbol
     ) ERC20(name, symbol) {}
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function burn(address owner, uint256 amount) public {
+    function burn(address owner, uint256 amount) public onlyOwner {
         _burn(owner, amount);
     }
 }
